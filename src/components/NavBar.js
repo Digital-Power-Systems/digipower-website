b88269de7593399155ade7fa63f45aaf3af83be1
Sanjay <sanjayrjs16@gmail.com>
2020-11-16 22:05:00 +0530

    Nav bar initital commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,23 @@
 import React, {useState} from 'react'
 import { Link} from 'react-router-dom';
 import DigiPowerLogo from  '../logo.svg'
+import styles from './NavBar.modules.css'
 export default function NavBar() {
     return (
       <>
-        <nav className="navbar">
-            <div className="navbar-container">
-                <Link to="/"><img src={DigiPowerLogo} /></Link>
-                <Link to="/services">Products</Link>
-                <Link to="/about">About</Link>
-                <Link to="/contact">Contact us</Link>
-            </div>
+        <nav className={styles.navbar}>
+           
+                <div className="navbar-logo">
+                  <Link to="/"><img src={DigiPowerLogo} /></Link>
+                </div>
+               <div className="navbar-items">
+                <ul>
+                  <li><Link to="/services">Products</Link></li>
+                  <li><Link to="/about">About</Link></li>
+                  <li> <Link to="/contact">Contact us</Link></li>
+                </ul>
+               </div>
+          
         </nav>
       </>
     )
