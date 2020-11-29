688718f0489767b42e4557234759a45ced47ca69
Sanjay <sanjayrjs16@gmail.com>
2020-11-29 20:33:28 +0530

    Changed inde to server.js

--- client/src/components/Product/ProductList.js
+++ client/src/components/Product/ProductList.js
@@ -2,6 +2,8 @@ import React from 'react';
 import cctv from '../../assets/SVGs/cctv.svg';
 import solar from '../../assets/SVGs/solar-energy.svg'
 import battery from '../../assets/SVGs/battery.svg'
+import inverter from '../../assets/SVGs/inverter.png'
+import ups from '../../assets/SVGs/UPS-icon.png'
 
 import './ProductList.css';
 
@@ -10,9 +12,11 @@ export default function ProductList() {
        <>
             <div className="product-list-container">
                 <h1> Explore our Power Products....</h1>
-                <span> <img src={cctv} width="70rem" height="70rem" alt="cctv-icon" /> CCTV Cameras</span>
+                <span> <img src={cctv} width="70rem" height="70rem" alt="cctv-icon" /> CCTV</span>
                 <span> <img src={solar} width="70rem" height="70rem" alt="cctv-icon" /> Solar Products</span>
                 <span> <img src={battery} width="70rem" height="70rem" alt="cctv-icon" /> Batteries</span>
+                <span> <img src={inverter} width="70rem" height="70rem" alt="cctv-icon" /> Inverter</span>
+                <span> <img src={ups} width="70rem" height="70rem" alt="cctv-icon" /> UPS</span>
             </div>
        </>
     )
