f28cf30e5fd9662f9200a8d14526954ae6bdeb7f
Sanjay Rajesh <36541242+sanjayrjs16@users.noreply.github.com>
2020-11-29 22:00:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 
