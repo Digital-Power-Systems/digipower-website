0edfa603feaa0dec4112919c8376d74200e05bdf
Sanjay <sanjayrjs16@gmail.com>
2020-11-30 23:47:46 +0530

    About page made responsive

--- client/src/components/Product/ProductList.js
+++ client/src/components/Product/ProductList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import exide from '../../assets/Battery/exide-banner.jpg'
 import cctv from '../../assets/SVGs/cctv.svg';
 import solar from '../../assets/SVGs/solar-energy.svg'
 import battery from '../../assets/SVGs/battery.svg'
@@ -14,16 +15,13 @@ export default function ProductList() {
        <>
                 <h1> Our Power Products</h1>
             <div className="product-list-container">
-                <ProductCard img={cctv} header="CCTV" description="Level up the security with  watchful eyes around."/>
-                <ProductCard img={solar} header="Solar products" description="Harness the power of the sun. Power your home "/>
-                <ProductCard img={battery} header="Batteries" description="The best batteries around for al your needs"/>
                 <ProductCard img={inverter} header="Inverters" description="Keep the power going with world-class inverters."/>
                 <ProductCard img={ups} header="UPS" description="Keep the power going with world-class UPS"/>
-                {/* <span> <img src={cctv}  alt="cctv-icon" /> CCTV</span>
-                <span> <img src={solar} width="70rem" height="70rem" alt="cctv-icon" /> Solar Products</span>
-                <span> <img src={battery} width="70rem" height="70rem" alt="cctv-icon" /> Batteries</span>
-                <span> <img src={inverter} width="70rem" height="70rem" alt="cctv-icon" /> Inverter</span>
-                <span> <img src={ups} width="70rem" height="70rem" alt="cctv-icon" /> UPS</span> */}
+                <ProductCard img={solar} header="Solar products" description="Harness the power of the sun to power your homes, protect  nature and keep the bills light.  "/>
+                <ProductCard img={battery} header="Batteries" description="The best batteries around for all your needs"/>
+                <ProductCard img={cctv} header="Surveilence cameras" description="Level up the security with  watchful eyes around."/>
+              
+              
             </div>
        </>
     )
