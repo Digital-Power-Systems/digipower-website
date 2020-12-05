739d2adb86c15518863ef289849bd6eda9bd7a09
Sanjay <sanjayrjs16@gmail.com>
2020-12-05 22:48:12 +0530

    Added URL based selection styles for product menu

--- client/src/components/Products/ProductsMenu/ProductMenu.js
+++ client/src/components/Products/ProductsMenu/ProductMenu.js
@@ -6,19 +6,27 @@ import battery from '../../../assets/SVGs/battery.svg';
 import inverter from '../../../assets/SVGs/inverter.png';
 import ups from '../../../assets/SVGs/UPS-icon.png';
 
-import {Link} from 'react-router-dom';
+import {useParams} from 'react-router-dom';
 
 import './ProductMenu.css';
 import ProductMenuItem from './ProductMenuItem';
-export default function ProductMenu() {
+export default function ProductMenu(props) {
+    const [selected, setSelected] =useState("?selected=inverter");
+ 
+    const menu = [{id: 1, description: "Inverter", URL: "?selected=inverter", img: inverter },
+                    {id: 2, description: "Online/Offline UPS", URL: "?selected=ups", img: ups },
+                    {id: 3,  description: "Solar products", URL: "?selected=solar", img: solar },
+                    {id: 4, description: "Batteries", URL: "?selected=battery", img: battery },
+                    {id: 5, description: "Security Cameras", URL: "?selected=cctv", img: cctv }
+                ];
+             
     return (
         <div className="product-menu-container">
-            
-               <ProductMenuItem description="Inverter" URL="/services?selected=inverter" img={inverter} selected={true}/>
-               <ProductMenuItem description="Online/Offline UPS" URL="/services?selected=ups" img={ups} selected={true}/>
-               <ProductMenuItem description="Solar products" URL="/services?selected=solar" img={solar} selected={true}/>
-               <ProductMenuItem description="Batteries" URL="/services?selected=battery" img={battery} selected={true}/>
-               <ProductMenuItem description="Security Cameras" URL="/services?selected=cctv" img={cctv} selected={true}/>
+                {menu.map((item) => {
+                    return <ProductMenuItem  description={item.description} URL={item.URL} img={item.img} selected={selected === item.URL} onClick={() => {setSelected(item.URL)}} />
+                })}
+           
+               
         </div>
     )
 }
