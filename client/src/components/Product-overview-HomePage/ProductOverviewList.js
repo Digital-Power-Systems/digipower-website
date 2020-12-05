a7d44a3b4d4f5a6ed8fa16afdd007fc1b64121e3
Sanjay <sanjayrjs16@gmail.com>
2020-12-06 01:37:56 +0530

    Added redux

--- client/src/components/Product-overview-HomePage/ProductOverviewList.js
+++ client/src/components/Product-overview-HomePage/ProductOverviewList.js
@@ -8,7 +8,7 @@ import inverter from '../../assets/SVGs/inverter.png'
 import ups from '../../assets/SVGs/UPS-icon.png'
 
 import './ProductOverviewList.css';
-import ProductCard from './ProductOverviewCard';
+import ProductOverviewCard from './ProductOverviewCard';
 
 
 export default function ProductOverviewList() {
@@ -16,11 +16,11 @@ export default function ProductOverviewList() {
        <>
                 <h1> Our Power Products</h1>
             <div className="product-overview-list-container">
-                <ProductCard img={inverter} header="Inverters" description="Keep the power going with world-class inverters." URL="/services?selected=inverter"/>
-                <ProductCard img={ups} header="Online/Offline UPS" description="Keep the power going with world-class UPS" URL="/services?selected=ups"/>
-                <ProductCard img={solar} header="Solar products" description="Harness the power of the sun to power your homes, protect  nature and keep the bills light." URL="/services?selected=solar"/>
-                <ProductCard img={battery} header="Batteries" description="The best batteries around for all your needs" URL="/services?selected=battery"/>
-                <ProductCard img={cctv} header="Surveilence cameras" description="Level up the security with  watchful eyes around." URL="/services?selected=cctv"/>
+                <ProductOverviewCard img={inverter} header="Inverters" description="Keep the power going with world-class inverters." URL="/services?selected=inverter"/>
+                <ProductOverviewCard img={ups} header="Online/Offline UPS" description="Keep the power going with world-class UPS" URL="/services?selected=ups"/>
+                <ProductOverviewCard img={solar} header="Solar products" description="Harness the power of the sun to power your homes, protect  nature and keep the bills light." URL="/services?selected=solar"/>
+                <ProductOverviewCard img={battery} header="Batteries" description="The best batteries around for all your needs" URL="/services?selected=battery"/>
+                <ProductOverviewCard img={cctv} header="Surveilence cameras" description="Level up the security with  watchful eyes around." URL="/services?selected=cctv"/>
               
               
             </div>
