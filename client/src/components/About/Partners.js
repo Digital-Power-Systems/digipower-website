0d5e9831e588beb97d265f457ca7cc46b942a45d
Sanjay <sanjayrjs16@gmail.com>
2020-12-10 02:12:03 +0530

    Added all partner image links, open in new tab

--- client/src/components/About/Partners.js
+++ client/src/components/About/Partners.js
@@ -18,15 +18,15 @@ export default function Partners() {
                  <h1>Our partners</h1>
                  <hr />
                 <div className="partner-list">
-                    <div><img src={livfastLogo}  className="partner-logo" alt="livfastlogo"/></div>
-                    <div><img src={microLogo}  className="partner-logo" alt="amaronlogo"/></div>
-                    <div><img src={UTL}  className="partner-logo" alt="rrlogo"/></div>
-                    <div><img src={wareeLogo}  className="partner-logo" alt="wareelogo"/></div>
-                    <div><img src={livguardLogo}  className="partner-logo" alt="livguardlogo"/></div>
-                    <div><img src={sayokLogo}  className="partner-logo" alt="sayoklogo"/></div>
-                    <div><img src={altronLogo}  className="partner-logo" alt="amaronlogo"/></div>
-                    <div><img src={exideLogo}  className="partner-logo" alt="exidelogo"/></div>  
-                    <div><img src={amaronLogo}  className="partner-logo" alt="amaronlogo"/></div>
+                    <div><a href="https://www.livfast.in/About/" target="_blank" rel="noopener noreferrer"><img src={livfastLogo}  className="partner-logo" alt="livfastlogo"/></a></div>
+                    <div><a href="https://www.rrmicrobatteries.com/" target="_blank"><img src={microLogo}  className="partner-logo" alt="micrologo"/></a></div>
+                    <div><a href="https://www.upsinverter.com/" target="_blank"><img src={UTL}  className="partner-logo" alt="UTLlogo"/></a></div>
+                    <div><a href="https://www.waaree.com/" target="_blank"><img src={wareeLogo}  className="partner-logo" alt="wareelogo"/></a></div>
+                    <div><a href="https://www.livguard.com/about.php" target="_blank"><img src={livguardLogo}  className="partner-logo" alt="livguardlogo"/></a></div>
+                    <div><a href="http://www.sayokbatteries.com/" target="_blank"><img src={sayokLogo}  className="partner-logo" alt="sayoklogo"/></a></div>
+                    <div><a href="http://www.altron.in/" target="_blank"><img src={altronLogo}  className="partner-logo" alt="altronlogo"/></a></div>
+                    <div><a href="https://www.exide.com/en" target="_blank"><img src={exideLogo}  className="partner-logo" alt="exidelogo"/></a></div>  
+                    <div><a href="https://www.amaron.in/" target="_blank"><img src={amaronLogo}  className="partner-logo" alt="amaronlogo"/></a></div>
                 </div>
                 <hr />
         </div>
