847cff7651aba3b4d39727258a64f26c53fa93b7
Sanjay <sanjayrjs16@gmail.com>
2020-12-09 23:33:12 +0530

    Dope 2.0, product overview list

--- client/src/components/Product-overview-HomePage/ProductOverviewList.js
+++ client/src/components/Product-overview-HomePage/ProductOverviewList.js
@@ -14,12 +14,12 @@ import ProductOverviewCard from './ProductOverviewCard';
 export default function ProductOverviewList() {
     return (
        <>
-                <h1  className="product-overview-header"> Our Power Products</h1>
+                <h1  className="product-overview-mainheader"> Our Power Products</h1>
             <div className="product-overview-list-container">
-                <ProductOverviewCard img={inverter} header="Inverters" description="Backup power with world-classinverters." URL="/services?selected=inverter"/>
-                <ProductOverviewCard img={ups} header="Online UPS" description="Keep the power going with world-class UPS" URL="/services?selected=onlineups"/>
-                <ProductOverviewCard img={ups} header="Home UPS" description="Keep the power going with world-class UPS" URL="/services?selected=ups"/>
-                <ProductOverviewCard img={solar} header="Solar products" description="Harness the power of the sun to power your homes, protect  nature and keep the bills light." URL="/services?selected=solar"/>
+                <ProductOverviewCard img={inverter} header="Inverters" description="Power backup to keep the vibe on with world-class inverters." URL="/services?selected=inverter"/>
+                <ProductOverviewCard img={ups} header="Online UPS" description="Focus on work and keep the power going with home UPS" URL="/services?selected=onlineups"/>
+                <ProductOverviewCard img={ups} header="Home UPS" description="World-Class online UPS with power when you need it" URL="/services?selected=ups"/>
+                <ProductOverviewCard img={solar} header="Solar products" description="Harness the power of the sun, protect nature and keep the bills light." URL="/services?selected=solar"/>
                 <ProductOverviewCard img={battery} header="Batteries" description="The best batteries around for all your needs" URL="/services?selected=battery"/>
                 <ProductOverviewCard img={cctv} header="Surveilence cameras" description="Level up the security with  watchful eyes around." URL="/services?selected=cctv"/>
               
